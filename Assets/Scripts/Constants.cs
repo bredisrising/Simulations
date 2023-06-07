de05af9d26337e7bc315ca33c4c3e35bf92cce8e
bredisrising <ankitmalhotra788@gmail.com>
2023-06-06 17:55:15 -0700

    gravity works

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public static float ActualG = 6.67430f;
+    public static float GScaler = 1;
+    public static float G = ActualG * GScaler;
+
+    public static float Scale = 1000.0f;
+    public static float DistanceScale = 100000.0f;
+}
---
+++ Assets/Scripts/GravitySubject.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySubject : MonoBehaviour
+{
+    static List<GravitySubject> gravitySubjects;
+    Rigidbody rb;
+
+    [SerializeField] Vector3 initialVelocity = Vector3.zero;
+
+	private void OnEnable()
+	{
+        if (gravitySubjects == null)
+        {
+			gravitySubjects = new List<GravitySubject>();
+		}
+		gravitySubjects.Add(this);
+	}
+
+	void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(initialVelocity, ForceMode.VelocityChange);
+    }
+
+	private void FixedUpdate()
+	{
+	    foreach (GravitySubject subject in gravitySubjects)
+        {
+            if (subject != this)
+                DoGravity(subject);   
+        }	
+	}
+
+
+    void DoGravity(GravitySubject subject)
+    {
+        Vector3 direction = subject.rb.position - rb.position;
+        float distance = direction.magnitude;
+
+        float gForce = (subject.rb.mass * rb.mass) / Mathf.Pow(distance, 2) * Constants.G;
+
+        Vector3 gVector = direction.normalized * gForce;
+
+        rb.AddForce(gVector);
+    }
+}
